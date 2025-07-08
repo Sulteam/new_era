a02bc7550ded1f2ca8da416b0f1ef066dc76d628
Sulteam <svc12@tpu.ru>
2025-07-08 11:07:22 +0300

    a

--- rtl/decoder_manch.v
+++ rtl/decoder_manch.v
@@ -16,26 +16,34 @@ module decoder_manch #(
 
   localparam FULLBAUD =  CLK_FREQ / BAUDRATE;
   localparam bit_width = $clog2(FULLBAUD);
+  localparam HALFBAUD = FULLBAUD / 2;
 
-  reg [bit_width - 1 :0] clk_counter; 
+
+  // детектирование фронтов и спадов принимаемого сигнала 
+  reg prev_data;
+  wire rising_edge  = (prev_data == 1'b0) && (rx_data == 1'b1);
+  wire falling_edge = (prev_data == 1'b1) && (rx_data == 1'b0);
+
+
+  reg [16 - 1 :0] clk_counter;
   reg CLK_TX;
 
+
   always @(posedge clk) begin
-    clk_counter  <= clk_counter + 1;
-  end
-
-  always @(posedge rx_data) begin
-    if ( clk_counter >= FULLBAUD - 1) begin
-      data_tx <= 1'b1;
-      clk_counter <= 0;  
+    clk_counter <= clk_counter + 1;
+    prev_data <= rx_data;
+    if ( rising_edge == 1'b1) begin
+      if ( clk_counter >= FULLBAUD + HALFBAUD  && clk_counter <= FULLBAUD + FULLBAUD + FULLBAUD) begin
+        data_tx     <= 1'b1;
+        clk_counter <= 0;  
+      end  
+    end else if (falling_edge == 1'b1) begin
+      if ( clk_counter >= FULLBAUD + HALFBAUD  && clk_counter <= FULLBAUD + FULLBAUD + FULLBAUD) begin
+        data_tx     <= 1'b0;
+        clk_counter <= 0;  
+      end       
     end
   end
 
-  always @(negedge rx_data) begin
-    if ( clk_counter >= FULLBAUD - 1) begin
-      data_tx <= 1'b0;
-      clk_counter <= 0;  
-    end
-  end
 
 endmodule
